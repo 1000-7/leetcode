17f9be117d66429320dd253dc404c00b2db9652d
UncleWang <xin1996whu@icloud.com>
2019-02-24 15:33:51 +0800

    Initial commit

---
+++ src/main/java/two/LinkedListCycle.java
@@ -0,0 +1,32 @@
+
+//Follow up:
+//Can you solve it without using extra space?
+
+
+public class LinkedListCycle {
+    public boolean hasCycle(ListNode head) {
+        if (head == null || head.next == null) {
+            return false;
+        }
+
+        ListNode slow = head;
+        ListNode fast = head.next;
+        while (fast != null && fast.next != null && fast != slow) {
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+
+        return fast == slow;
+    }
+
+    class ListNode {
+        int val;
+        ListNode next;
+
+        ListNode(int x) {
+            val = x;
+            next = null;
+        }
+    }
+
+}
