b1ad5ae86db14d80d886c4484d7c7d7843cbaa9e
wangxin <x.wang@whu.edu.cn>
2019-02-24 22:45:12 +0800

    代码更新

--- src/main/java/array/MaximumSubarray.java
+++ src/main/java/array/MaximumSubarray.java
@@ -6,23 +6,23 @@ package array;// Find the contiguous subarray within an array (containing at lea
 public class MaximumSubarray {
 
     public int maxSubArray(int[] nums) {
-        
+
         int[] dp = new int[nums.length];
-        
+
         dp[0] = nums[0];
-        
+
         int max = dp[0];
-        
-        for(int i = 1; i < nums.length; i++) {
-            
+
+        for (int i = 1; i < nums.length; i++) {
+
             dp[i] = nums[i] + (dp[i - 1] > 0 ? dp[i - 1] : 0);
-            
+
             max = Math.max(dp[i], max);
-            
+
         }
-        
+
         return max;
-        
+
     }
 
 }
